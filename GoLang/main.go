5cf5cf80e834fc2c3a147352bee4fe5f3562c76e
Tanzeel <111998938+TanzeelAkhther@users.noreply.github.com>
2025-01-11 21:08:32 +0000

    My First Go Program

---
+++ GoLang/main.go
@@ -0,0 +1,9 @@
+package main		//Every Go program must start with the package declaration
+
+import "fmt"		//fmt package is short form of format
+					//It implements formatting for input and output
+
+func main() {		//"main"- Building block of Go programs	and entry point of the executable program
+	fmt.Println("Hello World!")  //P insted of p
+}
+//dot operator between fmt and Println is known as the member access operator.
